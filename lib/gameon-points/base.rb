2d054e5a32740df6171f1deb6eaf81d9d238fde4
theotherstupidguy <theotherstupidguy@gmail.com>
2014-08-04 04:09:54 +0200

    adds more specs, seems complete

--- lib/gameon-points/base.rb
+++ lib/gameon-points/base.rb
@@ -1,26 +1,6 @@
-=begin
 GameOn::Env.register do
   attr_accessor :points
   def add_points value
-    if !@points.nil?
-      @points = @points + value
-    else 
-      @points = 0
-    end
-  end
-  def remove_points value
-    if !@points.nil?
-      @points = @points - value
-    else 
-      @points = 0
-    end
-  end
-end
-=end
-GameOn::Env.register do
-  attr_accessor :points
-  def add_points value
-    p "coooool"
     if @points.nil?
       @points = value
     else
@@ -29,9 +9,9 @@ GameOn::Env.register do
   end
   def remove_points value
     if @points.nil?
-      @points = value
+      @points = -(value)
     else
-      @points -= value
+      @points += -(value)
     end
   end
 end
@@ -39,13 +19,13 @@ end
 module GameOn
   class Points  
     include GameOn::Middleware 
-    #def initialize(app, opts= GameOn::Points::Opts, params= GameOn::Points::Params)
+
     def initialize(app, opts={},params={})
       @app = app
       @opts = opts 
       @params = params
-      #@points = 0
     end
+
     def call(env)
       if !@params.nil? && @params.has_key?(:add)
 	env[:gameon].add_points(@params[:add] * @opts[:inc_by]) 
--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -2,24 +2,35 @@ require_relative './spec_helper'
 
 describe "a Ruby Application 'GoMaster' that applies DDD 2.0" do
   before do
+    @app_key = "secret_key used by domain frameworks for encryptions"
     @user_id = "07"
+  end
+
+  after do 
     redis = Redis.new
     redis.del @user_id + 'gameon' 
   end
+
   it 'is a GameOn Middleware' do
-    skip
     GameOn::Points.include?(GameOn::Middleware).must_equal true
   end
 
-  it 'adds points using {:add => 2}' do 
+  #describe "GameOn's Gamebook" do
+  #  it 'extends GameOn::DSL' do
+  #    GoMaster::Gamebook.singleton_class.include?(GameOn::DSL).must_equal true
+  #  end
+  #end
+
+  it 'adds points using GameOn::Points::Params[:addition]' do 
+    GameOn::Points::Params[:addition] = {:add => 2}  
     module GoMaster 
       module Gamebook 
 	extend GameOn::DSL
-	points = {:inc_by => 1, :dec_by => 1 }
+	points = {:inc_by => 2, :dec_by => 1 }
 
 	context :good_mayor do 
 	  statment [:user, :visits, :good_page_one] do 
-	    activation GameOn::Points, points, {:add => 2}
+	    activation GameOn::Points, points, GameOn::Points::Params[:addition] 
 	  end
 	end
       end
@@ -29,19 +40,86 @@ describe "a Ruby Application 'GoMaster' that applies DDD 2.0" do
 	activity [:user, :visits, :good_page_one]
       end
     end
+    @gameon.points.must_equal 2 * 2 
+  end
+
+  
+  it 'adds points using {:add => 2}' do 
+    module GoMaster 
+      module Gamebook 
+	extend GameOn::DSL
+	points = {:inc_by => 1, :dec_by => 1 }
+
+	context :good_mayor do 
+	  statment [:user, :visits, :good_page_two] do 
+	    activation GameOn::Points, points, {:add => 2}
+	  end
+	end
+      end
+    end
+
+    @gameon = GameOn::Env.set @user_id do 
+      on :good_mayor do 
+	activity [:user, :visits, :good_page_two]
+      end
+    end
+
     @gameon.points.must_equal 2 
   end
 
-  it 'gets a GameOn activity' do 
-    skip
-    @app_key = "secret_key used by domain frameworks for encryptions"
-    @gameon = GameOn::Env.get @user_id 
-    @gameon.id.must_equal @user_id + "gameon"
+  it 'removes points using GameOn::Points::Params[:subtraction]' do 
+    GameOn::Points::Params[:subtraction] = {:remove => 2}  
+    module GoMaster 
+      module Gamebook 
+	extend GameOn::DSL
+	opts = {:inc_by => 1, :dec_by => 2 } #or GameOn::Points::Opts[:opt_1] =  {:inc_by => 2, :dec_by => 1 }
+
+	context :bad_mayor do 
+	  statment [:user, :visits, :bad_page_one] do 
+	    activation GameOn::Points, opts, GameOn::Points::Params[:subtraction] 
+	  end
+	end
+      end
+    end
+    @gameon = GameOn::Env.set @user_id do 
+      on :bad_mayor do 
+	activity [:user, :visits, :bad_page_one]
+      end
+    end
+    @gameon.points.must_equal -2 * 2 
   end
 
-  describe "GameOn's Gamebook" do
-    it 'extends GameOn::DSL' do
-      GoMaster::Gamebook.singleton_class.include?(GameOn::DSL).must_equal true
+  it 'removes points using {:subtraction => 2}' do 
+    module GoMaster 
+      module Gamebook 
+	extend GameOn::DSL
+	opts = {:inc_by => 1, :dec_by => 1} #or GameOn::Points::Opts[:opt_1] =  {:inc_by => 2, :dec_by => 1 }
+
+	context :bad_mayor do 
+	  statment [:user, :visits, :bad_page_two] do 
+	    activation GameOn::Points, opts, {:remove => 2}
+	  end
+	end
+      end
     end
+
+    @gameon = GameOn::Env.set @user_id do 
+      on :bad_mayor do 
+	activity [:user, :visits, :bad_page_two]
+      end
+    end
+
+    @gameon.points.must_equal -2 
+  end
+
+  it 'sets & gets a GameOn #id' do 
+    GameOn::Env.set @user_id do 
+      on :good_mayor do 
+	activity [:user, :visits, :good_page_two]
+      end
+    end
+
+    @gameon = GameOn::Env.get @user_id 
+    @gameon.id.must_equal @user_id + "gameon"
   end
 end
